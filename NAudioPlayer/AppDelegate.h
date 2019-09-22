04d2397175fc445c9a3bf7d6668bc6775ae36ffe
泽娄 <970649831@qq.com>
2019-09-22 17:29:48 +0800

    Initial Commit

---
+++ NAudioPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NAudioPlayer
+//
+//  Created by 泽娄 on 2019/9/22.
+//  Copyright © 2019 泽娄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
